f7bfa676bdad947fc7a1e0764551a8b47b897604
Zubir <zubir@zubir.dev>
2019-09-15 18:45:00 -0400

    stats script

--- scripts/civ6dump.js
+++ scripts/civ6dump.js
@@ -16,27 +16,28 @@ dumpdb = async () => {
 	console.log(turnlog);
 };
 
-get_turns = async discord_id => {
-	const turnlog = await db.findAll("civ6_turns", {discord_id});
-	return turnlog;
-};
-
-avg = (turns) => {
+avg = (turns, discord_id) => {
 	let sum = 0;
-	for(let i = 1; i < turns.length; i++){
-		sum += turns[i].timestamp - turns[i-1].timestamp;
+	let count = 0;
+	for(let i = 0; i < turns.length - 1; i++){
+		if(turns[i].discord_id === discord_id){
+			sum += (turns[i+1].timestamp - turns[i].timestamp) / 1000 / 60;
+			count += 1;
+		}
 	}
-	return sum / turns.length / 1000 / 60;
+	console.log(count);
+	return sum / count; //minutes
 };
 
 all_avg = async () => {
-	const nick = await get_turns(mapping["capitalism is bad fellow gamers"]);
-	const drek = await get_turns(mapping["R8 my R8 M8"]);
-	const zubir = await get_turns(mapping["Sir Flexalot"]);
+	const turns = await db.findAll("civ6_turns");
+	const nick = avg(turns, mapping["capitalism is bad fellow gamers"]);
+	const drek = avg(turns, mapping["R8 my R8 M8"]);
+	const zubir = avg(turns, mapping["Sir Flexalot"]);
 
-	console.log(`Nick:  ${avg(nick)}`);
-	console.log(`Drek:  ${avg(drek)}`);
-	console.log(`Zubir: ${avg(zubir)}`);
+	console.log(`Nick:  ${nick}`);
+	console.log(`Drek:  ${drek}`);
+	console.log(`Zubir: ${zubir}`);
 };
 
 //get_turns(mapping["capitalism is bad fellow gamers"]).then().catch(err => console.log(err));
